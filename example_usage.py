e5bc6ef6cbc22ec6a2ecd8767a72acb5c3aa8b0d
andrehuang <andrehuang0@gmail.com>
2025-05-03 10:38:35 +0200

    modify example_usage.py

--- example_usage.py
+++ example_usage.py
@@ -8,13 +8,13 @@ from PIL import Image, ImageOps
 import torchvision.transforms as T
 import torchvision.transforms.functional as TF
 
-model =  torch.hub.load('facebookresearch/dinov2', 'dinov2_vits14').to('cuda')
-kernel_size = 14
-lr_size = 16
+model, patch_size, dim = get_featurizer("dinov2")
+model = model.to('cuda')
+kernel_size = patch_size 
+lr_size = 224 // patch_size
 load_size = 224
 
-upsampler_path = "/path/to/loftup_dinov2.ckpt"
-upsampler = load_loftup_checkpoint(upsampler_path, 384, lr_pe_type="sine")
+upsampler = torch.hub.load('andrehuang/loftup', "loftup_dinov2s", pretrained=True)
 upsampler = upsampler.to('cuda')
 
 image_path = "examples/sa_1.jpg"
@@ -26,10 +26,10 @@ transform = T.Compose([
         norm])
 img = Image.open(image_path).convert("RGB")
 normalized_img_tensor = transform(img).unsqueeze(0).to('cuda')
-lr_feats = model.get_intermediate_layers(normalized_img_tensor, reshape=True)[0] # 1, 384, 14, 14
+lr_feats = model(normalized_img_tensor) # 1, dim, lr_size, lr_size
 
 ## Upsampling step
-hr_feats = upsampler(lr_feats, normalized_img_tensor) # 1, 384, 224, 224
+hr_feats = upsampler(lr_feats, normalized_img_tensor) # 1, dim, 224, 224
 
 # ## You can also upsample to any shape you want; Just change the guidance image shape
 # H, W = 112, 112
@@ -37,12 +37,3 @@ hr_feats = upsampler(lr_feats, normalized_img_tensor) # 1, 384, 224, 224
 # hr_feats_112 = upsampler(lr_feats, img_tensor_112) # 1, 384, 112, 112
 
 plot_feats(unnorm(normalized_img_tensor)[0], lr_feats[0], hr_feats[0], 'examples/feats.png')
-
-
-#### Alternative usage with featurizer ####
-
-model = get_featurizer("dinov2")
-lr_feats = model(normalized_img_tensor)
-hr_feats = upsampler(lr_feats, normalized_img_tensor)
-
-## This should give the same result as the original usage
