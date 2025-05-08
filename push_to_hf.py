5f265714f8ae2954e6d31ff1718d7e50b5eacc3b
andrehuang <andrehuang0@gmail.com>
2025-05-08 08:56:04 +0200

    update push to hf

--- push_to_hf.py
+++ push_to_hf.py
@@ -6,28 +6,28 @@ import torch
 
 # push loftup-dinov2s
 model = torch.hub.load('nielsrogge/loftup:add_hf', 'loftup_dinov2s', pretrained=True)
-model.upsampler.push_to_hub("nielsr/loftup-dinov2s")
+model.upsampler.push_to_hub("haiwen/loftup-dinov2s")
 
 # push loftup-siglip2
 model = torch.hub.load('nielsrogge/loftup:add_hf', 'loftup_siglip2', pretrained=True)
-model.upsampler.push_to_hub("nielsr/loftup-siglip2")
+model.upsampler.push_to_hub("haiwen/loftup-siglip2")
 
 # push loftup-siglip
 model = torch.hub.load('nielsrogge/loftup:add_hf', 'loftup_siglip', pretrained=True)
-model.upsampler.push_to_hub("nielsr/loftup-siglip")
+model.upsampler.push_to_hub("haiwen/loftup-siglip")
 
 # push loftup-dinov2b
 model = torch.hub.load('nielsrogge/loftup:add_hf', 'loftup_dinov2b', pretrained=True)
-model.upsampler.push_to_hub("nielsr/loftup-dinov2b")
+model.upsampler.push_to_hub("haiwen/loftup-dinov2b")
 
 # push loftup-dinov2s_reg
 model = torch.hub.load('nielsrogge/loftup:add_hf', 'loftup_dinov2s_reg', pretrained=True)
-model.upsampler.push_to_hub("nielsr/loftup-dinov2s_reg")
+model.upsampler.push_to_hub("haiwen/loftup-dinov2s_reg")
 
 # push loftup-dinov2b_reg
 model = torch.hub.load('nielsrogge/loftup:add_hf', 'loftup_dinov2b_reg', pretrained=True)
-model.upsampler.push_to_hub("nielsr/loftup-dinov2b_reg")
+model.upsampler.push_to_hub("haiwen/loftup-dinov2b_reg")
 
 # push loftup-clip
 model = torch.hub.load('nielsrogge/loftup:add_hf', 'loftup_clip', pretrained=True)
-model.upsampler.push_to_hub("nielsr/loftup-clip")
+model.upsampler.push_to_hub("haiwen/loftup-clip")
